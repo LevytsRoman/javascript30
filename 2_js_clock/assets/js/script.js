0387a5545c5d5944fa52412a88a2dca06b9d1682
levytsroman <r.levyts@gmail.com>
2017-03-28 17:41:36 -0400

    set up starter files

---
+++ 2_js_clock/assets/js/script.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+  // need to get time
+
+  //convert it to hours minutes and seconds
+
+  //convert each of the above into degrees
+
+  // make a function to fire every second and update all of the above 
+}
