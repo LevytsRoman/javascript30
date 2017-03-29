a718131ac046aeb6341a3a6caa0a96f938c11f5b
levytsroman <r.levyts@gmail.com>
2017-03-28 20:07:54 -0400

    adjust styles

--- 2_js_clock/assets/js/script.js
+++ 2_js_clock/assets/js/script.js
@@ -1,47 +1,34 @@
 window.onload = function(){
   // need to get time
-  var digital = document.querySelector('div'),
+  var digital = document.querySelector('.digital'),
       hourHand = document.querySelector('.hour-hand'),
       minuteHand = document.querySelector('.min-hand'),
-      secondHand = document.querySelector('.second-hand'),
-      // debugger
-      dialLength = hourHand.offsetWidth,
-      hourLeft = hourHand.getBoundingClientRect().left,
-      hourTop = hourHand.getBoundingClientRect().top,
-      minLeft = minuteHand.getBoundingClientRect().left,
-      minTop = minuteHand.getBoundingClientRect().top,
-      secLeft = secondHand.getBoundingClientRect().left,
-      secTop = secondHand.getBoundingClientRect().top;
+      secondHand = document.querySelector('.second-hand');
+
+  function prettyTime(m,h,s){
+    var min = m.toString().length === 1 ? `0${m}` : `${m}`,
+        hr = h.toString().length === 1 ? `0${h}` : `${h}`,
+        sec = s.toString().length === 1 ? `0${s}` : `${s}`;
+
+    return `${hr}:${min}:${sec}`;
+  }
 
   setInterval(updateTime, 1000);
-  console.log(hourLeft);
 
   function updateTime(){
     var currentTime = new Date,
         hours = currentTime.getHours() > 12 ? currentTime.getHours() - 12 : currentTime.getHours(),
         minutes = currentTime.getMinutes(),
-        seconds = currentTime.getSeconds();
-        digital.innerHTML =`${hours}:${minutes}:${seconds}`,
+        seconds = currentTime.getSeconds(),
         hourDeg = (hours * 360)/12 + 90,
         minDeg = (minutes * 360)/60 + 90,
-        secDeg = (seconds * 360)/60 + 90,
-        hourX = dialLength * Math.sin(hourDeg),
-        hourY = dialLength * Math.cos(hourDeg),
-        minX = dialLength * Math.sin(minDeg),
-        minY = dialLength * Math.cos(minDeg),
-        secX = dialLength * Math.sin(secDeg),
-        secY = dialLength * Math.cos(secDeg);
+        secDeg = (seconds * 360)/60 + 90;
+    digital.innerHTML = prettyTime(hours, minutes, seconds);
 
         // set css transform: rotate(deg); for each of the above
-      hourHand.style.transform = `rotate(${hourDeg}deg)`;
-      minuteHand.style.transform = `rotate(${minDeg}deg)`;
-      secondHand.style.transform = `rotate(${secDeg}deg)`;
-      hourHand.style.left = `${hourLeft + hourX}px`;
-      hourHand.style.top = `${hourTop + hourY}px`;
-      minuteHand.style.left = `${minLeft + minX}px`
-      minuteHand.style.top = `${minTop + minY}px`
-      secondHand.style.left = `${secLeft + secX}px`
-      secondHand.style.top = `${secTop + secY}px`
+    hourHand.style.transform = `rotate(${hourDeg}deg)`;
+    minuteHand.style.transform = `rotate(${minDeg}deg)`;
+    secondHand.style.transform = `rotate(${secDeg}deg)`;
   }
 
   // make a function to fire every second and update all of the above
