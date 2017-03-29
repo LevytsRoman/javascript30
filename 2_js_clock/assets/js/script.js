2ad0e86cdf2fcb319b2bc62a14cc812f69a2d01f
levytsroman <r.levyts@gmail.com>
2017-03-29 16:11:04 -0400

    change how degrees are obtained to have a smooth transition from 59sec/min to 0, advust styles

--- 2_js_clock/assets/js/script.js
+++ 2_js_clock/assets/js/script.js
@@ -3,6 +3,13 @@ window.onload = function(){
       hourHand = document.querySelector('.hour-hand'),
       minHand = document.querySelector('.min-hand'),
       secHand = document.querySelector('.second-hand');
+      currentTime = new Date,
+      hours = currentTime.getHours() > 12 ? currentTime.getHours() - 12 : currentTime.getHours(),
+      minutes = currentTime.getMinutes(),
+      seconds = currentTime.getSeconds(),
+      hourDeg = (hours * 360)/12 + 90 + 30,
+      minDeg = (minutes * 360)/60 + 90 + 6,
+      secDeg = (seconds * 360)/60 + 90 + 6;
 
   function prettyTime(h,m,s){
     var hr = convertTime(h),
@@ -17,29 +24,24 @@ window.onload = function(){
   }
 
   function rotate(el, deg){
-    if(deg === 90){
-      el.style.transitionDuration = '0s';
-      el.style.transform = `rotate(${deg}deg)`;
-    }else {
-      el.style.transitionDuration = '1s';
-      el.style.transform = `rotate(${deg}deg)`;
-    }
+    el.style.transform = `rotate(${deg}deg)`;
   }
 
   function updateTime(){
     var currentTime = new Date,
         hours = currentTime.getHours() > 12 ? currentTime.getHours() - 12 : currentTime.getHours(),
         minutes = currentTime.getMinutes(),
-        seconds = currentTime.getSeconds(),
-        hourDeg = (hours * 360)/12 + 90,
-        minDeg = (minutes * 360)/60 + 90,
-        secDeg = (seconds * 360)/60 + 90;
+        seconds = currentTime.getSeconds();
 
     digital.innerHTML = prettyTime(hours, minutes, seconds);
-    // set css transform: rotate(deg); for each of the above
+
     rotate(hourHand, hourDeg);
     rotate(minHand, minDeg);
     rotate(secHand, secDeg);
+
+    hourDeg += 360/60/60/12
+    minDeg += 360/60/60
+    secDeg += 360/60
   }
 
   setInterval(updateTime, 1000);
